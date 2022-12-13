4a6dbf9ea76f44ea70c585fa5c5cf6ed299b2109
shareef99 <nadeemshareef934@gmail.com>
2022-12-13 11:10:36 +0530

    Update employee schema

--- src/Employee/Employee.model.ts
+++ src/Employee/Employee.model.ts
@@ -1,5 +1,85 @@
 import mongoose from "mongoose";
 
+const personalDetailsSchema = new mongoose.Schema({
+  gender: {
+    type: String,
+    enum: {
+      values: ["male", "female", "others"],
+    },
+  },
+  dob: Date,
+  married: Boolean,
+  marriedDate: Date,
+  address: String,
+  bankAccountNo: Number,
+  bankName: String,
+  IFSCCode: String,
+  pancardNo: String,
+  pfNo: String,
+  aadharNo: String,
+  RTGSNo: String,
+});
+
+const qualificationDetailsSchema = new mongoose.Schema({
+  ssc: {
+    board: String,
+    yearOfPassing: Number,
+    percentage: Number,
+  },
+  inter: {
+    board: String,
+    yearOfPassing: Number,
+    percentage: Number,
+  },
+  degree: {
+    qualification: String,
+    branch: String,
+    yearOfPassing: Number,
+    percentage: Number,
+    remarks: String,
+  },
+  pg: [
+    {
+      qualification: String,
+      specification: String,
+      yearOfPassing: Number,
+      percentage: Number,
+      remarks: String,
+      grade: String,
+    },
+  ],
+  other: [
+    {
+      qualification: String,
+      specification: String,
+      yearOfPassing: Number,
+      percentage: Number,
+      remarks: String,
+      grade: String,
+    },
+  ],
+});
+
+const experienceSchema = new mongoose.Schema({
+  institute: String,
+  type: String,
+  from: Date,
+  to: Date,
+  designation: String,
+  subjects: [String],
+  salary: Number,
+});
+
+const familySchema = new mongoose.Schema({
+  memberName: String,
+  relation: String,
+  dob: Date,
+  age: Number,
+  aadharNo: Number,
+  insuranceName: String,
+  insuranceNo: String,
+});
+
 const employeeSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -74,6 +154,10 @@ const employeeSchema = new mongoose.Schema({
     ],
     default: [],
   },
+  personalDetails: personalDetailsSchema,
+  qualificationDetails: qualificationDetailsSchema,
+  experience: [experienceSchema],
+  familyDetails: [familySchema],
 });
 
 export default mongoose.model("Employees", employeeSchema);
